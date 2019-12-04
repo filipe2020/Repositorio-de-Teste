38b01187840504fc684d9d80a7d26919c826c4b0
filipeenzo20 <filipeenzo2020@gmail.com>
2019-12-04 20:37:25 -0300

    CursoJS

---
+++ CursoJS/Aula11/ex008.js
@@ -0,0 +1,12 @@
+var velocidade = 50
+console.log(`A velocidade do seu carro é ${velocidade}km/h`)
+
+//Condição simple
+if(velocidade > 80){
+   console.log(`Você utrapassou a velocidade permitida. MULTADO!`)
+}
+
+if(velocidade <= 50){
+    console.log(`Você é um bom condutor. Parabens !!!`)
+}
+
